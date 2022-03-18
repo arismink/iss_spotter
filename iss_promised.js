2c78d11d8bb88c3ac0852cea3599dee9ea716a51
krismina la <krismina.la@gmail.com>
2022-03-18 05:22:26 +0000

    code of promises to be called by index2.js

---
+++ iss_promised.js
@@ -0,0 +1,30 @@
+const request = require('request-promise-native');
+
+const fetchMyIP = function() {
+  return request('https://api.ipify.org?format=json');
+};
+
+const fetchCoordsByIP = function(ipBody) {
+  const ip = JSON.parse(ipBody).ip;
+
+  return request(`https://freegeoip.app/json/${ip}`);
+}
+
+const fetchISSFlyOverTimes = function(body) {
+  const { latitude, longitude } = JSON.parse(body);
+
+  return request(`https://iss-pass.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`);
+
+}
+
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+  .then(fetchCoordsByIP)
+  .then(fetchISSFlyOverTimes)
+  .then(body => {
+    const { response } = JSON.parse(body);
+    return response;
+  });
+}
+
+module.exports = { nextISSTimesForMyLocation };
